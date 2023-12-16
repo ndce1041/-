685886a65a754c57a71ce4071df67c021438b62b
Xujinh <xujinh-1@foxmail.com>
2023-12-16 19:38:18 +0800

    a

--- model/LSTM.py
+++ model/LSTM.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import math
 import matplotlib.pyplot as plt
 from core.data_processor import DataLoader
@@ -7,7 +8,14 @@ from core.model import Model
 import pandas as pd
 import numpy as np
 
-def model(data):
+
+def model(data,input={
+        "train_test_split": 0.8,
+        "nero_num": 100,
+        "dropout_rate": 0.2,
+        "seq_len": 50,
+    }):
+    
     df=data
     #转换为Date,Open,High,Low,Close,Volume
     df = df.sort_values(by='trade_date', ascending=True)
@@ -22,15 +30,27 @@ def model(data):
     path = 'data/stoke.csv'
     df.to_csv(path,index=False)
 
+
     #修改config.json中的参数
     with open('config.json') as f:
         config = json.load(f)
         #data.filename
         config['data']['filename'] = 'stoke.csv'
         #data.train_test_split
-        config['data']['train_test_split'] = 1 #全部用于训练,一次性预测
+        config['data']['train_test_split'] = input['train_test_split']
         #data.sequence_length
-        config['data']['sequence_length'] = 50 #一次性预测
+        config['data']['sequence_length'] = input['seq_len']
+        config["model"]["layers"][0]["input_timesteps"] = input['seq_len']-1
+        #dropout_rate,nero_num
+        #所有的dropout_rate都改,所有的nero_num都改
+        for i in range(len(config["model"]["layers"])):
+            if(config["model"]["layers"][i]["type"]=="dropout"):
+                config["model"]["layers"][i]["rate"] = input['dropout_rate']
+            elif(config["model"]["layers"][i]["type"]=="lstm"):
+                config["model"]["layers"][i]["neurons"] = input['nero_num']
+
+
+
 
     #保存修改后的config.json
     with open('config.json','w') as f:
@@ -82,28 +102,35 @@ def model(data):
     )
 
 
+
     #预测
-    #有修改
+    #计算准确率
     #取最后一段长为sequence_length的数据
+    #1by1
     x_test=x[-configs['data']['sequence_length']:]
     y_test=y[-configs['data']['sequence_length']:]
-    print(x_test.shape)
-    print(y_test.shape)
-    print(y_test)
-    # x_test, y_test = data.get_test_data(
-    #     seq_len=configs['data']['sequence_length'],
-    #     normalise=configs['data']['normalise']
-    # )
+    predictions = model.predict_point_by_point(x_test)
+    mse_last_seq_day = np.mean(np.square(predictions - y_test))
+    print("LSTM:最后一段长为sequence_length的数据的mse为:{}".format(mse_last_seq_day))
+
+    #预测结果(往后sequence_length天的预测结果)
     predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
-    # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
-    # predictions = model.predict_point_by_point(x_test)
-    predictions = predictions[0]
+    predictions = np.array(predictions)[0]
+    predictions = predictions[0:21]
+    #plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+
+
+    #predictions = predictions[0]
     #只根据预测结果的第一天和预测结果的平均值,返回1(涨)或者0(跌)
     average = np.average(predictions)
     print("LSTM:预测结果的平均值(normalised)为:{}".format(average))
-    if(predictions[0]<average):
+    print("LSTM:预测结果的第一天(normalised)为:{}".format(predictions[0]))
+
+    if(predictions[0]>average):
         print("LSTM:预测结果为:涨")
-        return 1
+        return 1,mse_last_seq_day,predictions.tolist()
+        
     else:
         print("LSTM:预测结果为:跌")
-        return 0
+        return 0,mse_last_seq_day,predictions.tolist()
+
--- model/arima.py
+++ model/arima.py
@@ -1,17 +1,47 @@
 from statsmodels.tsa.arima.model import ARIMA
 import statsmodels.api as sm
+import numpy as np
+from sklearn.metrics import mean_squared_error
 
-def model(data):
-    data = data['close']
-    
-    # Fit ARIMA model
-    model = ARIMA(data, order=(1, 0, 0))
-    model_fit = model.fit()
-    
-    # Predict next day's data
-    next_day_prediction = model_fit.predict(len(data), len(data))
-    
-    # Compare with last day's data
-    last_day_data = data.iloc[-1]    
-
-    return int(next_day_prediction.item() > last_day_data.item())
+def model(df,config):
+    a = config["a"]
+    b = config["b"]
+    c = config["c"]
+    m = config["m"]
+    # a, b, c是ARIMA模型的参数,a是自回归项,b是差分项,c是移动平均项
+    # m是训练集的占比
+    # df是数据集
+    # 返回模型的训练结果和预测结果
+    df = df['close'].values
+    train_size = int(len(df) * m)
+    train, test = df[0:train_size], df[train_size:]
+    history = [x for x in train]
+    predictions = list()
+    for t in range(len(test)):
+        model = ARIMA(history, order=(a, b, c))
+        model_fit = model.fit()
+        output = model_fit.forecast()
+        yhat = output[0]
+        predictions.append(yhat)
+        obs = test[t]
+        history.append(obs)
+    error = mean_squared_error(test, predictions)
+    print('Test MSE: %.3f' % error)
+    if error < 1:
+        print('模型表现较好')
+    else:
+        print('模型表现较差')
+    # plt.plot(test)
+    # plt.plot(predictions, color='red')
+    # plt.show()
+    #预测接下来20天的数据
+    forecast = model_fit.forecast(20)
+    # plt.plot(forecast)
+    # plt.show()
+    print("预测接下来20天的数据：",forecast)
+    #若下个20天的平均值大于今天的收盘价，则买入，否则卖出
+    # if np.mean(forecast[0])>df[-1]:
+    #     print(1)
+    # else:
+    #     print(0)
+    return int(np.mean(forecast[0])>df[-1]),error,forecast
